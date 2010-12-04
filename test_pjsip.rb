a7f97a1b57440c086f52f27d31e1de78c19b1828
andrerigon <andre.rigon@gmail.com>
2010-12-04 16:41:33 -0200

    example

--- test_pjsip.rb
+++ test_pjsip.rb
@@ -3,6 +3,6 @@ require 'rpjsip'
 
 include Rpjsip
 
-account_id = pjsip_init "proxy.azzu.com.br", "azzu", "12013283", "12345"
-call account_id , "03496739006", "azzu"
+account_id = pjsip_init "<proxy>", "<domain>", "<user>", "<passwd>"
+call account_id , "<number>", "<domain>"
 puts pjsip_destroy
